df813ce383af767a16f717d8418a346a9405bfdc
Luke Parrott <lparrott@salesforce.com>
2018-07-27 15:21:20 -0500

    Fix for entering single scheduled payment, also disabling calculate button if no amount is entered

--- src/aura/giftPaymentScheduler/giftPaymentSchedulerController.js
+++ src/aura/giftPaymentScheduler/giftPaymentSchedulerController.js
@@ -55,5 +55,14 @@
 		}
 		component.set("v.payments", paymentList);
 		component.set("v.showPayments", true);
+	},
+	handleAmtChange: function(component, event, helper) {
+		var donationAmt = component.get("v.donationAmt");
+		var btn = component.find('calcButton');
+		// Enable the calculate button if valid amount is entered
+		if(btn){
+			var validAmount = (donationAmt > 0);
+			btn.set('v.disabled',!validAmount);
+		}
 	}
 })

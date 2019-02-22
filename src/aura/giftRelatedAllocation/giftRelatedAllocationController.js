a7d766c46068920e3f6fd6143c40a505c7ac282d
Luke Parrott <lparrott@salesforce.com>
2019-02-22 15:20:51 -0600

    Gift Entry form: rounding Allocation percentage to 2 decimal places, disabling Cancel button when Save button is disabled

--- src/aura/giftRelatedAllocation/giftRelatedAllocationController.js
+++ src/aura/giftRelatedAllocation/giftRelatedAllocationController.js
@@ -1,14 +1,19 @@
 ({
     handlePercentChange: function(component, event, helper){
         // Set the amount based on this percent so we can check totals
-        var percent = component.get('v.item.npsp__Percent__c');
-        if(!percent){
+        var percentInput = component.get('v.item.npsp__Percent__c');
+        if(!percentInput){
             component.set('v.item.npsp__Percent__c', null);
             return;
         }
+        
+        var percent = Math.round((+percentInput + 0.00001) * 100) / 100;
+        component.set('v.item.npsp__Percent__c', percent);
+
         var donationAmt = component.get('v.donationAmt');
         var amt = percent * donationAmt / 100;
         component.set('v.item.npsp__Amount__c', amt);
+
         // Also trigger a change event to check totals
         helper.amountCheck(component, event, helper);
     }

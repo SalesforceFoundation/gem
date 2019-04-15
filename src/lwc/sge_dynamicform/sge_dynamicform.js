bc33b8a2038fd31bcc49d4222d670a174e3761e0
Benjamin Dvorachek <bdvorachek@salesforce.com>
2019-04-15 12:25:11 -0500

    SGE LWC test changes

---
+++ src/lwc/sge_dynamicform/sge_dynamicform.js
@@ -0,0 +1,11 @@
+/**
+ * Created by bdvorachek on 2019-04-15.
+ */
+
+import {LightningElement, track} from 'lwc';
+//import * as layoutService from 'c/layoutService';
+
+export default class SGE_DynamicForm extends LightningElement {
+    @track layout;
+
+}

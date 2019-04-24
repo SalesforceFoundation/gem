deba6b42e4002b2f2b4058690269afc28dae7b59
Luke Parrott <lparrott@salesforce.com>
2019-04-24 09:24:14 -0500

    Preventing Payments from being both Paid and Written Off in Single Gift Entry

--- src/aura/giftRelatedPayment/giftRelatedPaymentController.js
+++ src/aura/giftRelatedPayment/giftRelatedPaymentController.js
@@ -1,21 +1,30 @@
 ({
 	handlePaid: function(component, event, helper) {
-		var paymentDate = component.find('paymentDate');
-		if(!paymentDate){
-			return;
-		}
-		var payDate = null;
+		var payDate = null;		
 		var isPaid = component.get('v.item.npe01__Paid__c');
 		if(isPaid){
 			payDate = component.get('v.item.npe01__Scheduled_Date__c');
+			// A Payment cannot be paid and written off
+			component.set('v.item.npe01__Written_Off__c', false);
 		}
-		// Set the hidden Payment Date field if marked paid
-		paymentDate.set('v.value', payDate);
+
+		var paymentDateField = component.find('paymentDate');
+        if(!paymentDateField){
+            // Set the hidden Payment Date field if marked paid
+            paymentDateField.set('v.value', payDate);
+        }
 
 		// Set the Payment Method field to 'required' if payment is paid
 		var methodInput = component.find('methodField');
 		if(methodInput){
 			methodInput.set("v.required", isPaid);
+		}
+	},
+	preventPaidAndWrittenOff: function(component, event, helper){
+		var isWrittenOff = event.getParam('value');
+		if(isWrittenOff){
+			// A Payment cannot be paid and written off
+			component.set('v.item.npe01__Paid__c', false);
 		}
 	},
 	clearAmount: function(component, event, helper){
